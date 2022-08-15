79d39f3a2bba74bd7f55792097f3ff3c7e339db5
Stanislav Pankrashin <stasera2@gmail.com>
2022-08-15 15:51:14 +1200

    fix it

--- problems/Problem7.ts
+++ problems/Problem7.ts
@@ -11,6 +11,7 @@ You can assume that the messages are decodable. For example, '001' is not allowe
  * SOLUTION NOTES:
  * When decoding a string, we can build a binary tree representing the strings to be decoded.
  * We can decode either one number or two
+ * If a branch is not valid, i.e if the number is greater than 26, we disregard the entire branch
  * e.g
  * "123":
  * |                          \
@@ -51,20 +52,21 @@ You can assume that the messages are decodable. For example, '001' is not allowe
     "Z",
 ];
 
-const decodeLetter = (letter: string) => {
-    return LETTER_MAPPING[parseInt(letter)];
+const decodeLetter = (letter: string): string | undefined => {
+    return LETTER_MAPPING?.[parseInt(letter)];
 }
 
 const decodeMessage = (message: string): string[] => {
     // if we just have one character, decode it and return
     if (message.length === 1) {
         const firstDecoded = decodeLetter(message[0]);
-        return [firstDecoded];        
+        return [firstDecoded ?? ""];        
     }
 
     // if we have two characters, we can decode each letter as one digit, and decode both digits as a 3rd letter, returning the result
     if(message.length === 2) {
-        const bothDecoded = decodeLetter(message);
+        const bothDecoded = decodeLetter(message) ?? "";
+        
         return [`${decodeLetter(message[0])}${decodeLetter(message[1])}`, bothDecoded];
     } 
 
@@ -76,12 +78,26 @@ const decodeMessage = (message: string): string[] => {
     const firstSlice = message.slice(1);
     const secondSlice = message.slice(2);
 
+    const branchOne = decodeMessage(firstSlice).map(e => e === "" ? "" : `${firstCharacter}${e}`).filter(e => e !== "");
+
+    // if the 2nd character is not valid, we cannot use it and return nothing for this branch. 
+    const branchTwo = !secondCharacter ? [] : decodeMessage(secondSlice).map(e => e === "" ? "" : `${secondCharacter}${e}`).filter(e => e !== "");
+
     // we iterate through all of the returned letters, appending the relevant decoded letter to the start of the result set for each returned letter
-    return [...decodeMessage(firstSlice).map(e => `${firstCharacter}${e}`), ...decodeMessage(secondSlice).map(e => `${secondCharacter}${e}`)];
+    return [...branchOne, ...branchTwo];
 }
 
-const message1 = "123";
+const message1 = "111";
 const decoded_message1 = decodeMessage(message1);
 
 console.log(`test 1 result: ${decoded_message1}`);
-console.log(decodeLetter("12"))
+
+const message2 = "76548";
+const decoded_message2 = decodeMessage(message2);
+
+console.log(`test 1 result: ${decoded_message2}`);
+
+const message3 = "123857391232";
+const decoded_message3 = decodeMessage(message3);
+
+console.log(`test 1 result: ${decoded_message3}`);
