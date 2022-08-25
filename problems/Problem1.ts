3213b10c637cbbb26ca5c9baf10270556bef397f
Stanislav Pankrashin <stasera2@gmail.com>
2022-08-26 11:54:57 +1200

    Problem1

--- problems/Problem1.ts
+++ problems/Problem1.ts
@@ -7,3 +7,30 @@ For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
 
 Bonus: Can you do this in one pass?
  */
+
+const TwoNumbers = (k: number, list: number[]): boolean => {
+    type index = number;
+    type value = number;
+    const numbersMap = new Map<value, index>();
+
+    for(let i = 0; i < list.length; i++) {
+        const value = list[i];
+        if (numbersMap.get(value) === undefined) {
+            numbersMap.set(value, i);
+        }
+
+        const valueToFind = k - value;
+        const foundValue = numbersMap.get(valueToFind);
+
+        if (foundValue !== undefined) {
+            return true;
+        }
+    }
+
+    return false;
+};
+
+
+const test1 = TwoNumbers(17, [10, 15, 3, 7]);
+
+console.log(`test1 pass: ${test1 === true}`);
